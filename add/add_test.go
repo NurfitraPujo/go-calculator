be6004255f3f5839b88e024abd3e731743cd54ae
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-09-05 13:20:06 +0700

    Refactor Add tests cases assertion

--- add/add_test.go
+++ add/add_test.go
@@ -7,16 +7,22 @@ func TestAdd(t *testing.T) {
 		sum := Add(2, 2)
 		expected := 4
 
-		if sum != expected {
-			t.Errorf("expected '%d' but got '%d'", expected, sum)
-		}
+		assertEqual(sum, expected)
 	})
 	t.Run("does return sum of array of arguments", func(t *testing.T) {
 		sum := Add(2, 2, 6)
 		expected := 10
 
-		if sum != expected {
-			t.Errorf("expected '%d' but got '%d'", expected, sum)
-		}
+		assertEqual(sum, expected)
 	})
+}
+
+func printError(got, want int, t *testing.T) {
+	t.Errorf("expected '%d' but got '%d'", want, got)
+}
+
+func assertEqual(got, want int) {
+	if got != want {
+		printError(got, want, &testing.T{})
+	}
 }
